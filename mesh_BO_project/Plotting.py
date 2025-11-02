6bfd7717221978344c9a2ca05d2a8645e7d2b950
aum3 <82110576+aum3@users.noreply.github.com>
2025-11-02 11:04:16 +0000

    commenting erro

--- mesh_BO_project/Plotting.py
+++ mesh_BO_project/Plotting.py
@@ -16,11 +16,10 @@ import re
 
 #####################################
 
-1) Each method is to take in a mesh, trace and then output a trace. 
-2) Keep in mind it's certain I'm gonna forget this plotly shit so keep that in mind when writing this code. 
-3) The cleanse_unpack is to allow for forgetting the ** when inputting plotting kwargs. This easy fix must be
-removed and forgetting ** must raise an error in case it causes major issues downstream
-'''
+#1) Each method is to take in a mesh, trace and then output a trace. 
+#2) Keep in mind it's certain I'm gonna forget this plotly shit so keep that in mind when writing this code. 
+#3) The cleanse_unpack is to allow for forgetting the ** when inputting plotting kwargs. This easy fix must be
+#removed and forgetting ** must raise an error in case it causes major issues downstream
 
 #####################################
 
